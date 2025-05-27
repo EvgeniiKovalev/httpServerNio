a4cdbd1fd26433774a66e62ae406612a17bfb0a9
Evgeniy Kovalev <ev-kov@mail.ru>
2025-05-27 06:53:12 +0300

    Revert "11"
    This reverts commit 10546af072dd10b08821d42b45aafb6d877b2f8c.

---
+++ src/main/java/http/server/processors/Utils.java
@@ -0,0 +1,30 @@
+package http.server.processors;
+
+import java.util.function.Function;
+
+public class Utils {
+
+//    public <T> T getAndValidateParam(
+//            String paramName,
+//            Function<String, T> funcParse,
+//            String errorFuncMessage
+//    ) {
+//        if (!containsParameter(paramName)) {
+//            throw new BadRequestException(
+//                    "INCORRECT_REQUEST_DATA",
+//                    "Missing \"" + paramName + "\" parameter"
+//            );
+//        }
+//
+//        String paramValue = getParameter(paramName);
+//        try {
+//            return funcParse.apply(paramValue);
+//        } catch (Exception e) {
+//            throw new BadRequestException(
+//                    "INCORRECT_REQUEST_PARAMETER",
+//                    "Параметр '" + paramName + "' " + errorFuncMessage
+//            );
+//        }
+//    }
+
+}
