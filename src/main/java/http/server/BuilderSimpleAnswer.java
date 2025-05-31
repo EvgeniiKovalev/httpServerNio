d32edb6eb152b9203806660cbb5b184e6c217c93
Evgeniy Kovalev <ev-kov@mail.ru>
2025-05-31 14:53:25 +0300

    simplified router, simplified handle errors add BuilderSimpleAnswer

---
+++ src/main/java/http/server/BuilderSimpleAnswer.java
@@ -0,0 +1,33 @@
+package http.server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BuilderSimpleAnswer {
+    private static final Logger logger = LogManager.getLogger(BuilderSimpleAnswer.class);
+
+    public static String headerBody(int statusCode, String errorCode, String body, String contentType) {
+        return String.format("%s%s",
+                header(statusCode, errorCode, contentType),
+                (body == null ?  "" :
+                        (contentType.equals("text/html") || contentType == null) ? String.format("<html><body><h1>%s</h1></body></html>", body) : body));
+    }
+
+    public static String header(int statusCode, String errorCode, String contentType) {
+
+        return String.format("HTTP/1.1 %d %s\r\nContent-Type: %s\r\n\r\n",
+                statusCode,
+                (errorCode != null ? errorCode :
+                        switch (statusCode) {
+                            case 200 -> "OK";
+                            case 201 -> "Created";
+                            case 204 -> "No Content";
+                            case 400 -> "Bad Request";
+                            case 404 -> "Not Found";
+                            default -> "Not Found";
+                        }),
+                (contentType != null ? contentType : "text/html")
+        );
+    }
+
+}
--- src/main/java/http/server/HttpServer.java
+++ src/main/java/http/server/HttpServer.java
@@ -18,6 +18,8 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer implements AutoCloseable {
     private static final Logger logger = LogManager.getLogger(HttpServer.class);
@@ -66,7 +68,8 @@ public class HttpServer implements AutoCloseable {
         selector = Selector.open();
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
         this.isRunning = true;
-        logger.info("server started, host: {},  порт: {}", HOST, PORT);
+        logger.info("server started, host: {},  порт: {}, макс. кол-во соединений: {}", HOST, PORT, MAX_CONNECTIONS);
+        this.pool = Executors.newFixedThreadPool(10);
     }
 
     public void run() throws Exception {
@@ -78,20 +81,22 @@ public class HttpServer implements AutoCloseable {
                 continue;
             }
             logger.trace("readyChannels: {} key(s)", readyChannels);
-            Iterator<SelectionKey> keysIterator = selector.selectedKeys().iterator();
+            var selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keysIterator = selectedKeys.iterator();
             while (keysIterator.hasNext()) {
                 SelectionKey key = keysIterator.next();
                 keysIterator.remove();
-                if (key.isAcceptable()) {
-                    accept(selector, serverChannel, key);
-                } else if (key.isReadable()) {
-                    read(key);
-                } else if (key.isWritable()) {
-                    write(key);
-                } else {
-                    logger.trace("Unknown type key: {}", key.toString());
+
+                if (!key.isValid()) continue;
+                int readyOps = key.readyOps();
+                switch (readyOps) {
+                    case SelectionKey.OP_ACCEPT -> accept(selector, serverChannel, key);
+                    case SelectionKey.OP_READ -> read(key);
+                    case SelectionKey.OP_WRITE -> write(key);
+                    default -> logger.warn("Unknown type readyOps {}, key : {}", readyOps, key);
                 }
             }
+            selectedKeys.clear();
         }
         logger.info("server stopped");
     }
@@ -110,38 +115,42 @@ public class HttpServer implements AutoCloseable {
             if (clientChannel == null) return;
             clientChannel.configureBlocking(false);
             clientChannel.register(selector, SelectionKey.OP_READ);
-            logger.debug("New clientChannel connected: {}", clientChannel.getRemoteAddress());
+            logger.trace("New clientChannel connected: {}", clientChannel.getRemoteAddress());
         } catch (IOException e) {
-            logger.error("Accept error: {}", e.getMessage(), e);
+            logger.error("Accept error ", e);
             closeConnection(clientChannel, key);
         }
     }
 
     private void closeConnection(SocketChannel clientChannel, SelectionKey key) {
+        logger.trace("closeConnection {}", key);
         try {
-            if (key != null && key.isValid()) {
-                try {
-                    Object attachment = key.attachment();
-                    if (attachment instanceof AutoCloseable) {
-                        ((AutoCloseable) attachment).close();
-                    }
-                } catch (Exception e) {
-                    logger.error("Error closing attachment: {}", e.getMessage(), e);
+            boolean ex;
+            ex = (key != null && key.isValid());
+            if (!ex) return;
+            try {
+                Object attachment = key.attachment();
+                if (attachment instanceof AutoCloseable) {
+                    ((AutoCloseable) attachment).close();
                 }
-                key.attach(null);
-                key.cancel();
+            } catch (Exception e) {
+                logger.error("Error closing attachment", e);
             }
             if (clientChannel != null && clientChannel.isOpen()) clientChannel.close();
         } catch (IOException e) {
-            logger.error("error closing connection: {}", e.getMessage(), e);
+            logger.error("error closing connection ", e);
+        } finally {
+            key.attach(null);
+            key.cancel();
         }
     }
 
     private void read(SelectionKey key) {
         logger.trace("read");
         if (key == null || !key.isValid()) return;
-
         SocketChannel clientChannel = (SocketChannel) key.channel();
+        if (!clientChannel.isOpen()) return;
+
         try {
             ByteBuffer inputByteBuffer = ByteBuffer.allocate(BUFFER_SIZE);
 
@@ -150,29 +159,36 @@ public class HttpServer implements AutoCloseable {
                 Context context = new Context();
                 context.setInputBuffer(inputByteBuffer);
                 context.incLengthRequest(bytesRead);
+                ParsingResult parsingResult;
                 if (context.getLengthRequest() <= MAX_HTTP_REQUEST_SIZE) {
-                    context.setParsingResult(RequestParser.parseToResult(inputByteBuffer));
+                    parsingResult = RequestParser.parseToResult(inputByteBuffer);
                 } else {
                     ErrorDto errorDto = ErrorFactory.createErrorDto(
                             HttpErrorType.BAD_REQUEST,
                             "ERROR_REQUEST_TOO_LARGE",
                             "Request size exceeds allowed limit (" + MAX_HTTP_REQUEST_SIZE + " bytes)"
                     );
-                    context.setParsingResult(ParsingResult.error(errorDto));
+                    parsingResult = ParsingResult.error(errorDto);
                 }
+                context.setParsingResult(parsingResult);
                 requestRouter.route(context, clientChannel, inputByteBuffer);
+                if (context.getRequestAnswer() == null)
+                    logger.debug("------------------------------context.getRequestAnswer() == null");
                 key.attach(context);
                 key.interestOps(SelectionKey.OP_WRITE);
                 selector.wakeup();
+                if (key.attachment() == null)
+                    logger.debug("1 key.attachment() == null");
             } else if (bytesRead == -1) {
                 closeConnection(clientChannel, key);
                 logger.trace("Connection closed by clientChannel");
+                if (key.attachment() == null) logger.debug(" 2 key.attachment() == null");
             } else {
                 closeConnection(clientChannel, key);
                 logger.warn("Invalid bytesRead: {}", bytesRead);
             }
         } catch (Exception e) {
-            logger.error("Read error: {}", e.getMessage(), e);
+            logger.error("Read error ", e);
             closeConnection(clientChannel, key);
         }
     }
@@ -187,11 +203,20 @@ public class HttpServer implements AutoCloseable {
         }
         try {
             Context context = (Context) key.attachment();
+            if (context == null) {
+                logger.trace("context == null");
+                return;
+            }
             RequestAnswer requestAnswer = context.getRequestAnswer();
-            ByteBuffer outputByteBuffer = requestAnswer.getByteBuffer();
-            clientChannel.write(outputByteBuffer);
+            ByteBuffer out = requestAnswer.getByteBuffer();
+
+            if ((key == null || !key.isValid() || !clientChannel.isOpen() || out.position() >= out.limit())) {
+                logger.trace("skipped ");
+                return;
+            }
+            clientChannel.write(out);
         } catch (Exception e) {
-            logger.error("Write outer error: {}", e.getMessage(), e);
+            logger.error("Write outer error", e);
         } finally {
             closeConnection(clientChannel, key);
         }
